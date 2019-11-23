355847de238159570b623d0fa09b47798a751040
Zachary Carreiro <43147444+Shadowlet@users.noreply.github.com>
2019-11-22 21:36:04 -0500

    SLider doesn't work.

---
+++ Assets/Scripts/ColorMenu.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ColorMenu : MonoBehaviour
+{
+    public Slider bodyColorSlider;
+    public Slider wheelColorSlider;
+
+    //private Renderer colorRend;
+
+    public Button startButton;
+
+    private float bodyValue = 0.03f;
+    private float wheelValue = 0.08f;
+    
+
+    void Start()
+    {
+        //colorRend = GetComponent<Renderer>();
+        bodyColorSlider.minValue = 0.1f;
+        bodyColorSlider.maxValue = 1.0f;
+        bodyColorSlider.value = bodyValue;
+
+        wheelColorSlider.minValue = 0.1f;
+        wheelColorSlider.maxValue = 1.0f;
+        wheelColorSlider.value = wheelValue;
+    }
+
+    public void ChangeColor()
+    {
+        bodyValue = bodyColorSlider.value;
+        wheelValue = wheelColorSlider.value;
+        //colorRend.material.SetColor("CarColor", new Color(bodyValue, wheelValue, 1));
+    }
+
+}
